3419ae958b5978e9348e56b9aa97c77de7900b72
bymaaivi <43798308+bymaaivi@users.noreply.github.com>
2018-10-03 20:32:31 -0300

    INICIAL
    Creacion clase , en donde mostramos en pantalla un hola Wold

---
+++ ClubHipico_Byron/src/cl/clubhipico/clubhipico_byron/HolaMundo.java
@@ -0,0 +1,9 @@
+package cl.clubhipico.clubhipico_byron;
+
+public class HolaMundo {
+	
+    public static void main (String[] args) {
+    	//le digo que imprima un texto EJM hola guaton byron
+    	System.out.println("Hola World");
+    }
+}
